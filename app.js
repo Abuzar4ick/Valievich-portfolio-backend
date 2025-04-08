11594d084b2cdb64f12e6db570b9df895e699267
Abuzar <abuzarrrakhmonov@gmail.com>
2025-04-08 23:26:41 +0500

    Add the cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const helmet = require('helmet')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
+app.use(cors())
 
 app.use('/api',
     require('./routes/ordersRoutes'),
